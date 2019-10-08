ec3485a1a8d455d28d875c7f9be709449032928d
kevinbh168 <kevin.brena@tecsup.edu.pe>
2019-10-07 23:46:02 -0500

    Lab 09 terminado final

---
+++ Lab09/Lab09/Lab09/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab09
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+
+            };
+            
+
+        }
+    }
+}
---
+++ Lab09/Lab09/Lab09/MonkeysPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab09
+{
+    public class MonkeysPageViewModel : ViewModelBase
+    {
+        public IList<Monkey> Monkeys => MonkeyData.Monkeys;
+
+        Monkey selectedMonkey;
+        public Monkey SelectedMonkey
+        {
+            get { return selectedMonkey; }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+
+}
